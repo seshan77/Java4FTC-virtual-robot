eb0ba782806d16dd5b3387fc472d8c6ab0d6981b
johnseybold <52759022+johnseybold@users.noreply.github.com>
2020-08-09 14:45:07 -0600

    Updated to Junit5.  Stripped out old Junit4 jar.  Added sample test.

---
+++ TeamCode/test/net/aspenk12/java4ftc/SampleTest.java
@@ -0,0 +1,14 @@
+package net.aspenk12.java4ftc;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SampleTest {
+
+    @Test
+    public void testSomething() {
+        String hello = "Hello";
+        assertEquals("Hello", hello);
+    }
+
+}
